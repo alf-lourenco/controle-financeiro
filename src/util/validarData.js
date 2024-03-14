e7fb5fe994b92c6bca77021c40123659dd1c8170
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-03-14 16:29:07 -0300

    diretorio para funcoes genericas

---
+++ src/util/validarData.js
@@ -0,0 +1,17 @@
+const validarData = (date, inicio) => {
+  let dataAtual = new Date(date);
+  let novaData = new Date(date);
+
+  novaData.setUTCMonth(novaData.getUTCMonth() + 1);
+  novaData.setUTCDate(inicio);
+
+  if (novaData.getUTCMonth() - dataAtual.getUTCMonth() > 1) {
+    dataAtual.setUTCMonth(dataAtual.getUTCMonth() + 1);
+    dataAtual.setUTCDate(0);
+
+    return dataAtual;
+  } else {
+    return novaData;
+  }
+};
+module.exports = validarData;
