28b8b785b69fa8cd42c85f1e75077c153908cb9f
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-03-20 19:33:23 -0300

    implementada a funcao isValidDiaMesAno

--- src/controllers/despesasController.js
+++ src/controllers/despesasController.js
@@ -1,32 +1,36 @@
 const cadastrarDespesa = require('../services/despesas/cadastrarDespesa.service.js');
 const deletarTodasDespesas = require('../services/despesas/deletarTodasDespesas.service.js');
 const listarDespesas = require('../services/despesas/listarDespesa.service.js');
-
+const isValidDiaMesAno = require('../util/isValidDiaMesAno.js');
 const despesasControllers = {
   create: async (req, res) => {
     try {
-      const response = await cadastrarDespesa(req.body);
-      res.status(201).json({ response });
+      if (!isValidDiaMesAno(req.body.vencimento)) {
+        return res.status(400).send('Data inválida. Insira data no formato "dd/mm/aaaa"');
+      } else {
+        const response = await cadastrarDespesa(req.body);
+        return res.status(201).json({ response });
+      }
     } catch (error) {
       console.log(error);
-      res.status(500).send(`Não foi possivel criar despesas. ${error}`);
+      return res.status(500).send(`Não foi possivel criar despesas. ${error}`);
     }
   },
 
   getAll: async (req, res) => {
     try {
       const response = await listarDespesas();
-      res.json(response);
+      return res.json(response);
     } catch (error) {
-      res.status(500).send('Não foi possivel realizar a operação.');
+      return res.status(500).send('Não foi possivel realizar a operação.');
     }
   },
   deleteAll: async (req, res) => {
     try {
       const response = await deletarTodasDespesas();
-      res.send('Arquivos deletados com sucesso');
+      return res.send('Arquivos deletados com sucesso');
     } catch (error) {
-      res.status(500).send('Não foi possivel realizar a operação.');
+      return res.status(500).send('Não foi possivel realizar a operação.');
     }
   },
 };
