e214a49606b8527bbb21c30539573b45b0d36ee2
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-03-21 15:00:52 -0300

    refatoracao do codigo

--- src/services/despesas/cadastrarDespesa.service.js
+++ src/services/despesas/cadastrarDespesa.service.js
@@ -1,52 +1,17 @@
-const verificaSituacao = require('../../util/verificaSituacao');
-const { v4: uuidv4 } = require('uuid');
-const calcularData = require('../../util/calcularData');
 const Despesa = require('../../model/Despesas');
-const validarData = require('../../util/validarData');
 const { AppError } = require('../../errors/appErrors');
+const criaParcelas = require('./criaParcelas.service');
 
-async function cadastrarDespesa(body) {
-  const { vencimento, atrasada, finalizada, pendente, nomeProduto, parcelas } = body;
-  const [dia, mes, ano] = vencimento.split('/');
-  let dataFormatada = validarData(dia, mes, ano);
+async function cadastrarDespesa(body, idComumParcelas = '') {
+  let parcelas = criaParcelas(body, idComumParcelas);
 
-  const idComum = uuidv4();
-  const diaInicio = dia;
-
-  let parcelaAtrasada = atrasada;
-  let parcelaFinalizada = finalizada;
-  let parcelaPendente = pendente;
-
-  for (let i = 1; i <= parcelas; i++) {
-    let situacao = verificaSituacao(parcelaAtrasada, parcelaFinalizada, parcelaPendente);
-
-    let parcela = { ...body, IdComumParcelas: idComum };
-    parcela.parcelas = i;
-    parcela.vencimento = dataFormatada.toLocaleDateString('pt-BR', { timeZone: 'UTC' });
-    parcela.situacao = situacao;
-    switch (situacao) {
-      case 'Atrasada':
-        parcelaAtrasada--;
-        break;
-      case 'Finalizada':
-        parcelaFinalizada--;
-        break;
-      case 'Pendente':
-        parcelaPendente--;
-        break;
-
-      default:
-        break;
-    }
-
-    try {
-      await Despesa.create(parcela);
-      dataFormatada = calcularData(dataFormatada, diaInicio);
-    } catch (error) {
-      throw new AppError();
-    }
+  try {
+    await Despesa.insertMany(parcelas);
+    return 'Cadastro realizado com sucesso';
+  } catch (error) {
+    throw new AppError();
   }
-  return 'Cadastro realizado com sucesso';
+
 }
 
 module.exports = cadastrarDespesa;
