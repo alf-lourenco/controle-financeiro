1c33a7196430597111c9520d936d977da4ade119
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-05-22 14:41:24 -0300

    correcao dos testes do arquivo criaParametrosBusca.test.js

--- test/criaParametrosBusca.test.js
+++ test/criaParametrosBusca.test.js
@@ -3,23 +3,22 @@ const { criaDataFim, parametrosParaBusca } = require('../src/util/criaParametros
 describe('teste para a funcao parametrosBusca', () => {
   test('Busca despesas Atradas ou Pendentes, datas definidas', () => {
     const exemplo = { situacao: new RegExp(/(Atrasada|Pendente)/, 'i'), vencimento: { $gte: '2024-01-31', $lte: '2024-06-10' } };
-    const response = parametrosParaBusca('2024-01-31', '2024-06-10', 'Atrasada', 'Pendente');
+    const response = parametrosParaBusca(['Atrasada', 'Pendente'], ['2024-01-31', '2024-06-10']);
     expect(response).toEqual(exemplo);
   });
   test('busca despesas Atrasadas ou Pendentes, data padrão', () => {
     const exemplo = { situacao: new RegExp(/(Atrasada|Pendente)/, 'i'), vencimento: { $lte: criaDataFim() } };
-    const response = parametrosParaBusca('', '', 'Atrasada', 'Pendente');
+    const response = parametrosParaBusca(['Atrasada', 'Pendente']);
     expect(response).toEqual(exemplo);
   });
   test('Busca despesas pendentes, datas definidas', () => {
     const exemplo = { situacao: new RegExp('Pendente'), vencimento: { $gte: '2024-01-01', $lte: '2024-12-31' } };
-    const response = parametrosParaBusca('2024-01-01', '2024-12-31', 'Pendente');
+    const response = parametrosParaBusca(['Pendente'], ['2024-01-01', '2024-12-31']);
     expect(response).toMatchObject(exemplo);
   });
   test('busca despesa pendente, data padrão', () => {
     const exemplo = { situacao: new RegExp('Penden') };
-    const response = parametrosParaBusca('', '', 'Pendente');
-    console.log('response ', response);
+    const response = parametrosParaBusca(['Pendente']);
     expect(response).toMatchObject(exemplo);
   });
 });
