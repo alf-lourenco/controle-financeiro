64eaf4e7ee2ac8d047140bf74b45543bb252326b
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-05-22 14:45:38 -0300

    implementacao do endpoint de busca de despesas pendentes

--- src/controllers/despesasController.js
+++ src/controllers/despesasController.js
@@ -2,7 +2,7 @@ const buscaPorParametro = require('../services/despesas/buscaPorParametro.servic
 const cadastrarDespesa = require('../services/despesas/cadastrarDespesa.service.js');
 const deletarTodasDespesas = require('../services/despesas/deletarTodasDespesas.service.js');
 const listarDespesas = require('../services/despesas/listarDespesa.service.js');
-const  parametrosParaBusca  = require('../util/criaDataInicioEFim.js');
+const { parametrosParaBusca } = require('../util/criaParametrosBusca.js');
 const isValidDiaMesAno = require('../util/isValidDiaMesAno.js');
 const despesasControllers = {
   create: async (req, res) => {
@@ -29,7 +29,17 @@ const despesasControllers = {
   },
   getAtrasadaPendente: async (req, res) => {
     try {
-      const parametros = parametrosParaBusca(req.body.dataInicio, req.body.dataFim);
+      const parametros = parametrosParaBusca(['Atrasada', 'Pendente'], [req.body.dataInicio, req.body.dataFim]);
+      const response = await buscaPorParametro(parametros);
+      return res.status(201).json(response);
+    } catch (error) {
+      return res.status(500).json({ msg: 'Não foi possivel realizar a operação', erro: error.message });
+    }
+  },
+  getAllPendentes: async (req, res) => {
+    try {
+      const { dataFim, dataInicio } = req.body;
+      const parametros = parametrosParaBusca(['Pendente'], [dataInicio, dataFim]);
       const response = await buscaPorParametro(parametros);
       return res.status(201).json(response);
     } catch (error) {
--- src/routers/despesas.routes.js
+++ src/routers/despesas.routes.js
@@ -2,17 +2,10 @@ const despesasControllers = require('../controllers/despesasController.js');
 const validarBodyMiddleware = require('../middleware/validarBody.middlewares.js');
 const despesa = require('express').Router();
 
-despesa.post('/', validarBodyMiddleware, async (req, res) => {
-  despesasControllers.create(req, res);
-});
-despesa.get('/todas', async (req, res) => {
-  despesasControllers.getAll(req, res);
-});
-despesa.delete('/', async (req, res) => {
-  despesasControllers.deleteAll(req, res);
-});
-despesa.get('/pendentes', async (req, res) => {
-  despesasControllers.getAtrasadaPendente(req, res);
-});
+despesa.post('/', validarBodyMiddleware, despesasControllers.create);
+despesa.get('/todas', despesasControllers.getAll);
+despesa.delete('/', despesasControllers.deleteAll);
+despesa.get('/pendenteseatrasadas', despesasControllers.getAtrasadaPendente);
+despesa.get('/pendentes', despesasControllers.getAllPendentes);
 
 module.exports = despesa;
